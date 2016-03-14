6ff85aba5972d401ca4d7d5fdbd786c2fa952674
Brendan Lewis <lewis2ba@dukes.jmu.edu>
2016-03-14 14:11:53 -0400

    added create method

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -3,4 +3,26 @@ class InvoicesController < ApplicationController
     @invoices = Invoice.all
     render json: @invoices.to_json, status: :ok
   end
+
+  def show
+    @invoice = Invoice.find(params[:id])
+    render json: @invoice.to_json, status: :ok
+  end
+
+  def create
+    @invoice = Invoice.new(invoice_params)
+
+    if @invoice.save
+      render json: @invoice.to_json
+    else
+      render json: @invoice.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def invoice_params
+    params.require(:invoice).permit(:invoice_date, :cost, :mowing_id)
+  end
+
 end
--- app/controllers/mowings_controller.rb
+++ app/controllers/mowings_controller.rb
@@ -3,4 +3,26 @@ class MowingsController < ApplicationController
     @mowings = Mowing.all
     render json: @mowings.to_json, status: :ok
   end
+
+  def show
+    @mowing = Mowing.find(params[:id])
+    render json: @mowing.to_json, status: :ok
+  end
+
+  def create
+    @mowing = Mowing.new(mowing_params)
+
+    if @mowing.save
+      render json: @mowing.to_json
+    else
+      render json: @mowing.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def mowing_params
+    params.require(:mowing).permit(:customer_id,:technician_id, :mow_date)
+  end
+
 end
