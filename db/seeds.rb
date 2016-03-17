07098532e62abe775628589f46a5f8386cb7a597
Brendan Lewis <lewis2ba@dukes.jmu.edu>
2016-03-17 00:42:30 -0400

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,28 +16,44 @@ technician1.save!
 technician2 = Technician.create(name:'John Legend')
 technician2.save!
 
+technician3 = Technician.create(name:'David Bowie')
+technician3.save!
+
 customer1 = Customer.create(name:'Brendan Lewis', address:'1234 Fake Street', lawn_size:100, technician_id: technician1.id)
 customer1.save!
 
-customer2 = Customer.create(name:'John Lewis', address:'9999 Art Street', lawn_size:200, technician_id: technician1.id)
+customer2 = Customer.create(name:'George Bush', address:'9999 Art Street', lawn_size:200, technician_id: technician1.id)
 customer2.save!
 
-customer3 = Customer.create(name:'Frank Lewis', address:'9321 Main Street', lawn_size:300, technician_id: technician1.id)
+customer3 = Customer.create(name:'Barack Obama', address:'9321 Main Street', lawn_size:300, technician_id: technician1.id)
 customer3.save!
 
-customer4 = Customer.create(name:'Liam Lewis', address:'3848 Middle Street', lawn_size:400, technician_id: technician2.id)
+customer4 = Customer.create(name:'Bill Clinton', address:'3848 Middle Street', lawn_size:400, technician_id: technician2.id)
 customer4.save!
 
-customer5 = Customer.create(name:'George Lewis', address:'2922 Lee Street', lawn_size:500, technician_id: technician2.id)
+customer5 = Customer.create(name:'Jimmy Carter', address:'2922 Lee Street', lawn_size:500, technician_id: technician2.id)
 customer5.save!
 
-mowing1 = Mowing.create(technician_id:technician1.id, customer_id:customer1.id, mow_date:"2016/5/5")
+customer6 = Customer.create(name:'Ronald Reagan', address:'5134 Fleet Street', lawn_size:600, technician_id: technician2.id)
+customer6.save!
+
+customer7 = Customer.create(name:'George Washington', address:'9202 Spark Ave', lawn_size:700, technician_id: technician2.id)
+customer7.save!
+
+customer8 = Customer.create(name:'James Madison', address:'9429 Second Street', lawn_size:800, technician_id: technician2.id)
+customer8.save!
+
+mowing1 = Mowing.create(technician_id:technician1.id, customer_id:customer1.id, mow_date:"2016/3/1")
 mowing1.save!
 
-mowing2 = Mowing.create(technician_id:technician2.id, customer_id:customer2.id, mow_date:"2016/4/4")
+mowing2 = Mowing.create(technician_id:technician2.id, customer_id:customer2.id, mow_date:"2016/3/2")
 mowing2.save!
 
-mowing3 = Mowing.create(technician_id:technician2.id, customer_id:customer3.id, mow_date:"2016/6/6")
+mowing3 = Mowing.create(technician_id:technician2.id, customer_id:customer3.id, mow_date:"2016/3/3")
 mowing3.save!
 
-invoice1 = Invoice.create(mowing_id: mowing1.id, cost: 100)
+mowing4 = Mowing.create(technician_id:technician2.id, customer_id:customer4.id, mow_date:"2016/3/4")
+mowing4.save!
+
+mowing5 = Mowing.create(technician_id:technician3.id, customer_id:customer5.id, mow_date:"2016/3/5")
+mowing5.save!
